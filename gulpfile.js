614cc42364f68bc3e478fd49ed55c9b3c2a26fd0
Sam Rapaport <rapaport.sam7@gmail.com>
2015-12-23 23:11:44 -0800

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var elixir = require('laravel-elixir');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+// Place all Sass dependencies here
+var appSass = [
+    'main.scss'
+]
+
+// Place all Node JavaScript dependencies here
+var nodeScripts = [
+    'jquery/dist/jquery.min.js',
+    'bootstrap-sass/assets/javascripts/bootstrap.min.js',
+    'picturefill/dist/picturefill.min.js'
+];
+
+// Place all app JavaScript dependencies here
+var appScripts = [
+    'includes/*.js',
+    'main.js'
+];
+
+// Compile all the things
+elixir(function(mix) {
+    mix.sass(appSass, 'public/css/main.css')
+        .scripts(nodeScripts, 'public/js/nodeFrameworks.js', 'node_modules')
+        .scripts(appScripts, 'public/js/main.js');
+});
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Laravel Kickstart
+### Everyting you need to kick off a Laravel website
+___________
+### What is Laravel Kickstart?
+Laravel Kickstart is a Laravel starter project that helps you build Laravel websites faster. It borrows many aspects from Sage for Wordpress, combined with a little extra to create the perfect kickstart to your Laravel Website.
