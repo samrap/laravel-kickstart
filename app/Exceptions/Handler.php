ca5bda6dd9026a423b1c79a30df149082e8b5e79
Sam Rapaport <sam@gigasavvy.com>
2016-04-23 11:03:48 -0700

    CS fixes.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Exception;
+use \Illuminate\Http\Response;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -96,7 +97,7 @@ class Handler extends ExceptionHandler
             $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
         }
 
-        return new \Illuminate\Http\Response(
+        return new Response(
             $whoops->handleException($e),
             $e->getStatusCode(),
             $e->getHeaders()
