be81cdfb47298e104ab353dc21f696dbcac42122
sam <sam@samrapdev.com>
2016-06-23 22:57:24 -0700

    Add Kickstart command.

--- src/Kickstart/KickstartServiceProvider.php
+++ src/Kickstart/KickstartServiceProvider.php
@@ -15,4 +15,14 @@ class KickstartServiceProvider extends ServiceProvider
     {
         //
     }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->commands(['Samrap\Kickstart\Commands\Kickstart']);
+    }
 }
