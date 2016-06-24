fce8d0f7b9adba557fb30f0e14674375c03a243d
sam <sam@samrapdev.com>
2016-06-23 22:45:31 -0700

    Restructure project to be a Composer package.

---
+++ src/Kickstart/KickstartServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Samrap\Kickstart;
+
+use Illuminate\Support\ServiceProvider;
+
+class KickstartServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
